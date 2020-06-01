8fac184ce64005a2d244d9e7cf6d3692057e05b0
Boris Divjak <boris@divjak.net>
2020-06-01 21:59:28 +0100

    list top posters

--- controllers/twitter-controller.js
+++ controllers/twitter-controller.js
@@ -1,12 +1,27 @@
 var config = require('../config.js');
+var TwitterPost = require('../models/twitter-post.js');
+
+
 
 exports.twitterTrackerInfo = function(req, res) {
   // we're using == rather than === because tracker.id can be either number or string
   var tracker = config.twitter_trackers.filter( tracker => tracker.id == req.params['trackerId'] )[0];
   console.log(tracker);
 
-  res.render('tracker-info', { 
-      title:    tracker.name, 
-      tracker:  tracker
+  top_posters_query = TwitterPost[tracker.id].aggregate([
+    { $group: { _id: '$user.id_str', screen_name: { $first: '$user.screen_name' },  count: { $sum: 1 }}},
+    { $sort : { count : -1 } },
+    { $limit: 10 }
+  ]);
+  
+  Promise.all([top_posters_query]).then(([top_posters]) => {
+    res.render('tracker-info', { 
+        title:        tracker.name, 
+        tracker:      tracker,
+        top_posters:  top_posters
+    });
+  }).catch(err => {
+    console.log(err);
   });
+  
 };
