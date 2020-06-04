371444e874540adc943d1f988d8d6c17c2e3d6e0
Boris Divjak <boris@divjak.net>
2020-06-04 22:12:28 +0100

    update trackers

--- config.js
+++ config.js
@@ -1,26 +1,26 @@
 var hummmconfig = {
   twitter_trackers: [
     {
-      id:           1,
-      name:         'service design tracker',
+      id:           '1',
+      name:         'Service design UK',
       parameters: {
-        q:            '#servicedesign OR "service design"', 
+        q:            '#servicedesign OR "service design" OR "designing services" OR "designing public services" OR "digital discovery" OR "design research" OR "user research" OR "user centred" OR "human centred" OR "user-centred" OR "human-centred" OR "experience design"', 
         geocode:      '"52.5 -1.5 200km"'
       }
     },
     {
-      id:           2,
-      name:         'rails tracker',
+      id:           '2',
+      name:         'Ruby on Rails UK',
       parameters: {
-        q:            '"ruby on rails"', 
+        q:            '"ruby on rails" OR #rubyonrails', 
         geocode:      '"52.5 -1.5 200km"'
       }
     },            
     {
       id:           'ubxd',
-      name:         'from ubxd',
+      name:         'Unboxed tweets and mentions',
       parameters: {
-        q:            'from:ubxd', 
+        q:            'from:ubxd OR to:ubxd OR @ubxd', 
       }
     }            
   ]
