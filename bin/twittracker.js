9627363ac8ea34c1f53c2e2a064222a6fc5a38ec
Boris Divjak <boris@divjak.net>
2020-06-06 21:39:51 +0100

    move twitter calls into a separate js file

--- bin/twittracker.js
+++ bin/twittracker.js
@@ -5,68 +5,16 @@
 var db = require('../mongosetup.js');
 var TwitterPost = require('../models/twitter-post.js');
 var TwitterUser = require('../models/twitter-user.js');
-var Twitter = require('twit');
-var Strint = require('../util/strint.js'); 
+var Twitter = require('../util/twitter-calls');
 var config = require('../config.js')
- 
-// NOTE: we use Strint to process twitter IDs as strings, because they are too big for Numbers.
-// Javascript can only handle 53 bit integers, and Twitter IDs are 64 bit
- 
-var twit = new Twitter({
-  consumer_key:         process.env.TW_API_KEY,
-  consumer_secret:      process.env.TW_API_SECRET,
-  access_token:         process.env.TW_TOKEN,
-  access_token_secret:  process.env.TW_TOKEN_SECRET
-})
 
-var max_tweet_results = 10000; // limit calling the api at 100 calls (100 results x 100 calls = 10000)
-var count_per_call = 100; // 100 is the maximum for the standard API
-
-//
-//  function getTweets - make recursive calls to Twitter to get all relevant results (100 per call)
-//
-
-var getTweets = async (search_params, max_id) => {
-  if (typeof search_params !== 'object') {
-    return Promise.reject(new Error('getTweets requires search parameters for a twitter query'));
-  }
-
-  search_params.count       = count_per_call;
-  search_params.tweet_mode  = 'extended';
-  search_params.result_type = 'recent';
-  
-  if (max_id != undefined) search_params.max_id = max_id;
-    
-  // get first 100 tweets 
-  var tw_call_1 = twit.get('search/tweets', search_params);
-  
-  // if there's more, call API again to get next 'page'
-  var tw_call_2 = tw_call_1.then( result => {
-    if (result.data.statuses.length > 0 || result.data.statuses.length > max_tweet_results) {
-      // get min_id from all current ids and pass that to next twitter call (through a recursive getTweets) as max_id
-      var ids = result.data.statuses.map( status => status.id_str );
-      var min_id = ids.reduce( (a, b) => { return Strint.lt(a,b) ? a : b; });
-
-      return getTweets(search_params, Strint.sub(min_id,'1'));
-    }
-    else return result;
-  });
-
-  // merge statuses from this and from next call to twitter API
-  return Promise.all([tw_call_1, tw_call_2]).then( ([result_1, result_2]) => {
-      var new_statuses = [ ...result_1.data.statuses, ...result_2.data.statuses ];      
-      result_1.data.statuses = new_statuses;
-      return result_1;
-  });
-  
-}
 
 
 //
-//  function processTracker - retreive new tweets and users for each tracker
-//
+//  process all trackers - retreive new tweets and users for each tracker
+//  
 
-var processTracker = async (tracker) => {
+Promise.all( config.twitter_trackers.map( async tracker => {
   var users = []; // we'll need this to store users temporarily (to simplify promise logic)
   var user_ids = [];
   
@@ -86,8 +34,8 @@ var processTracker = async (tracker) => {
   
     // GET NEW TWEETS AND SAVE THEM
       
-    // get tweets - see the getTweets function above
-    var result = await getTweets(tracker.parameters);
+    // get tweets from twitter
+    var result = await Twitter.getTweets(tracker.parameters);
   
     // after twitter call, check for exsiting tweets (IDs) – check if any duplicates are already saved in database  
     var existing_posts = await TwitterPost[tracker.id].find({'id_str': { $in: result.data.statuses.map( status => status.id_str) }}, 'id_str');
@@ -123,6 +71,7 @@ var processTracker = async (tracker) => {
     // save remaining users to database
     var saved_users = await TwitterUser.insertMany(new_users);
     console.log('NEW USERS CREATED: ' + saved_users.length);
+
     
     
     // UPDATE EXISTING USERS
@@ -150,17 +99,7 @@ var processTracker = async (tracker) => {
   catch(err) {
     console.log(err.stack);
   }
-}
+}))
 
-
-
-//  MAIN - call twitter then write to database – repeat for each tracker defined in config.js
-
-var tracker_promises = [];
-
-config.twitter_trackers.forEach( tracker => {
-  tracker_promises.push(processTracker(tracker));
-}); 
-
-// close mongodb at the end of all calls
-Promise.all(tracker_promises).then(() => { db.close(); });
+// close mongodb at the end of all calls (when promises fulfilled)
+.then(() => { db.close(); });
